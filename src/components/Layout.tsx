2dc8065ef9261c512800038eeba8dcfda38ed305
Radseq <radek.gom@gmail.com>
2023-04-01 17:37:32 +0200

    feat: add layout component to maintain same layout to all pages, move navigation bar code to file

---
+++ src/components/Layout.tsx
@@ -0,0 +1,37 @@
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs"
+import { FC, ReactElement } from "react"
+import { NavigationBar } from "./homePage/NavigationBar"
+
+export const Layout: FC<{ children?: ReactElement }> = ({ children }) => {
+	const user = useUser()
+	return (
+		<div className="m-auto flex justify-center 2xl:w-3/5">
+			<main
+				className="h-full w-full flex-grow border-x-2 border-gray-200 p-4 
+								sm:w-full md:mr-4 md:w-2/3"
+			>
+				<header className="sticky">
+					<NavigationBar />
+				</header>
+				{children}
+			</main>
+			<aside
+				className="sticky hidden w-full flex-grow-0 rounded-lg border-2 border-gray-200 
+									sm:w-full md:block md:w-1/3"
+			>
+				<div className="sticky w-full rounded-xl p-4">
+					{!user.isSignedIn && (
+						<div>
+							<SignInButton />
+						</div>
+					)}
+					{user.isSignedIn && (
+						<div>
+							<SignOutButton />
+						</div>
+					)}
+				</div>
+			</aside>
+		</div>
+	)
+}
---
+++ src/components/homePage/NavigationBar.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link"
+import Image from "next/image"
+
+export const NavigationBar = () => {
+	return (
+		<ul className="w:48 flex content-center justify-between overflow-hidden">
+			<li className="rounded py-2 hover:bg-indigo-300">
+				<Link className="truncate" href="#">
+					<Image
+						width={10}
+						height={10}
+						src="https://cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/home.svg"
+						className="mx-4 inline w-7 sm:mx-2"
+						alt={"home icon"}
+					/>
+					<span className="hidden pr-3 sm:inline">Home</span>
+				</Link>
+			</li>
+			<li className="flex w-3/5 rounded py-2 hover:bg-indigo-300">
+				<Image
+					width={10}
+					height={10}
+					src="https://cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/search.svg"
+					className="mx-4 inline w-7 sm:mx-2"
+					alt={"search icon"}
+				/>
+				<input placeholder="Search Post" type="text" className="mr-3 w-full" />
+			</li>
+			<li className="rounded py-2 hover:bg-indigo-300">
+				<Link className="truncate" href="#">
+					<Image
+						width={10}
+						height={10}
+						src="https://cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/cog.svg"
+						className="mx-4 inline w-7 sm:mx-2"
+						alt={"cog icon"}
+					/>
+					<span className="hidden pr-3 sm:inline">Settings</span>
+				</Link>
+			</li>
+		</ul>
+	)
+}
