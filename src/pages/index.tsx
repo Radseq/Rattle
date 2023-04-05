4272cd8d3c7a3a08c5fa44c6a0f9e02e27142b60
Radseq <radek.gom@gmail.com>
2023-04-05 21:54:28 +0200

    fix: add head to every page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,6 @@ import { Layout } from "~/components/Layout"
 const Home: NextPage = () => {
 	return (
 		<>
-			<Head>
-				<title>Create T3 App</title>
-				<meta name="description" content="Rattle" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
 			<Layout></Layout>
 		</>
 	)
