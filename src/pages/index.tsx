43bd9c0bc62e3eb6046f3f93830e1846a6a22c8e
Radseq <radek.gom@gmail.com>
2023-04-05 21:56:32 +0200

    fix: error and warnings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,8 @@
 import { type NextPage } from "next"
-import Head from "next/head"
 import { Layout } from "~/components/Layout"
 
 const Home: NextPage = () => {
-	return (
-		<>
-			<Layout></Layout>
-		</>
-	)
+	return <Layout></Layout>
 }
 
 export default Home
