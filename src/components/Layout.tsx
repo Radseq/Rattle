8dce22beeb3b5ba5e6b89ea7160a6bb083dfbb8b
Radseq <radek.gom@gmail.com>
2023-04-11 14:12:13 +0200

    fix: layout sign out button to work on whole size

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -33,13 +33,15 @@ export const Layout = (props: PropsWithChildren) => {
 					{isSignedIn && (
 						<div>
 							<h1 className="p-2 text-2xl font-semibold">Hello {user.firstName}</h1>
-							<div
-								className="flex justify-center rounded border border-gray-500 bg-transparent 
+							<SignOutButton>
+								<span
+									className="flex justify-center rounded border border-gray-500 bg-transparent 
 							py-2 px-4 font-semibold text-gray-700 hover:border-transparent 
 							hover:bg-gray-500 hover:text-white"
-							>
-								<SignOutButton />
-							</div>
+								>
+									Sign Out
+								</span>
+							</SignOutButton>
 						</div>
 					)}
 				</div>
