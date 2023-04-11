cd7a999c2efe89c689db6381f81f43cdbeed6808
Radseq <radek.gom@gmail.com>
2023-04-11 21:50:54 +0200

    fix: middlewere sign in user navigate to home page as default page (root)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,26 @@
-import { withClerkMiddleware } from "@clerk/nextjs/server"
+import { getAuth, withClerkMiddleware } from "@clerk/nextjs/server"
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
 export default withClerkMiddleware((req: NextRequest) => {
+	const { userId } = getAuth(req)
+
+	if (req.nextUrl.pathname === "/" && userId) {
+		return NextResponse.redirect(new URL("/home", req.url))
+	}
 	return NextResponse.next()
 })
 
 // Stop Middleware running on static files
 export const config = {
-	matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+	matcher: [
+		/*
+		 * Match all request paths except for the ones starting with:
+		 * - _next
+		 * - static (static files)
+		 * - favicon.ico (favicon file)
+		 */
+		"/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)",
+		"/",
+	],
 }
