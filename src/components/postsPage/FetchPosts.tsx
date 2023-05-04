c25267e005dcc4f8cd3c39264aaef1c4a5fc3607
Radseq <radek.gom@gmail.com>
2023-05-04 15:36:06 +0200

    fix: add missing onNavigateToPost implementation

--- src/components/postsPage/FetchPosts.tsx
+++ src/components/postsPage/FetchPosts.tsx
@@ -2,10 +2,12 @@ import { type FC } from "react"
 import { api } from "~/utils/api"
 import { LoadingPage } from "../LoadingPage"
 import { PostItem } from "./PostItem"
+import { useRouter } from "next/router"
+import toast from "react-hot-toast"
 
 export const FetchPosts: FC<{ userId: string }> = ({ userId }) => {
 	const { data, isLoading } = api.posts.getAllByAuthorId.useQuery(userId)
-
+	const router = useRouter()
 	if (isLoading) {
 		return (
 			<div className="relative">
@@ -14,10 +16,25 @@ export const FetchPosts: FC<{ userId: string }> = ({ userId }) => {
 		)
 	}
 
+	const handleNavigateToPost = (postId: string, authorUsername: string) => {
+		// preventing navigate when user selecting text e.g post content text
+		if (!window.getSelection()?.toString()) {
+			router
+				.push(`/post/${authorUsername}/status/${postId}`)
+				.catch(() => toast.error("Error while navigation to post"))
+		}
+	}
+
 	return (
 		<ul className="">
 			{data?.map((postsWithUser) => (
-				<PostItem key={postsWithUser.post.id} postWithUser={postsWithUser} />
+				<PostItem
+					key={postsWithUser.post.id}
+					postWithUser={postsWithUser}
+					onNavigateToPost={() => {
+						handleNavigateToPost(postsWithUser.post.id, postsWithUser.author.username)
+					}}
+				/>
 			))}
 		</ul>
 	)
