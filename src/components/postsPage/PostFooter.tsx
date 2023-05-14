13a1016883e542dc9126551c5700def7ebe90b2d
Radseq <radek.gom@gmail.com>
2023-05-14 17:13:08 +0200

    fix: simplify post footer Icons

--- src/components/postsPage/PostFooter.tsx
+++ src/components/postsPage/PostFooter.tsx
@@ -1,23 +1,16 @@
-import { type FC, type PropsWithChildren, useState } from "react"
+import { type FC, type PropsWithChildren } from "react"
 import { Icon } from "../Icon"
 import { Heart } from "../Icons/Heart"
 import Link from "next/link"
 import { type PostWithUser } from "./types"
 
 const ReplayIcon = (props: PropsWithChildren) => {
-	const [hover, setHover] = useState<boolean>()
 	return (
-		<div
-			className="mr-2 flex"
-			onMouseEnter={() => {
-				setHover(true)
-			}}
-			onMouseLeave={() => setHover(false)}
-		>
-			<div className={`flex rounded-full p-1 ${hover ? "bg-blue-400" : ""}`}>
+		<div className="group mr-2 flex">
+			<div className={"flex rounded-full p-1 group-hover:bg-blue-400"}>
 				<Icon iconKind="chat" />
 			</div>
-			<span className={`self-center pl-1 text-xl ${hover ? "text-blue-400" : ""}`}>
+			<span className={"self-center pl-1 text-xl group-hover:text-blue-400"}>
 				{props.children}
 			</span>
 		</div>
@@ -29,21 +22,13 @@ type LikeIconProps = PropsWithChildren & {
 }
 
 const LikeIcon = (props: LikeIconProps) => {
-	const [hover, setHover] = useState<boolean>()
 	return (
-		<div
-			className="flex"
-			onMouseEnter={() => {
-				setHover(true)
-			}}
-			onMouseLeave={() => setHover(false)}
-		>
+		<div className="group flex">
 			<Heart
-				className={`h-9 w-9 rounded-full p-1 ${hover ? "bg-red-500" : ""}`}
+				className={"h-9 w-9 rounded-full p-1 group-hover:bg-red-500"}
 				fillColor={props.fillColor}
 			/>
-
-			<span className={`self-center pl-1 text-xl ${hover ? "text-red-500" : ""} `}>
+			<span className={"self-center pl-1 text-xl group-hover:text-red-500"}>
 				{props.children}
 			</span>
 		</div>
