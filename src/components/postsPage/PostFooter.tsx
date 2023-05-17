9891e49c56826a7a7f11b918e120e94a1304502f
Radseq <radek.gom@gmail.com>
2023-05-17 19:04:31 +0200

    fix: delete components and copy its code to post footer

--- src/components/postsPage/PostFooter.tsx
+++ src/components/postsPage/PostFooter.tsx
@@ -4,37 +4,6 @@ import { Heart } from "../Icons/Heart"
 import Link from "next/link"
 import { type PostWithUser } from "./types"
 
-const ReplayIcon = (props: PropsWithChildren) => {
-	return (
-		<div className="group mr-2 flex">
-			<div className={"flex rounded-full p-1 group-hover:bg-blue-400"}>
-				<Icon iconKind="chat" />
-			</div>
-			<span className={"self-center pl-1 text-xl group-hover:text-blue-400"}>
-				{props.children}
-			</span>
-		</div>
-	)
-}
-
-type LikeIconProps = PropsWithChildren & {
-	fillColor: string
-}
-
-const LikeIcon = (props: LikeIconProps) => {
-	return (
-		<div className="group flex">
-			<Heart
-				className={"h-9 w-9 rounded-full p-1 group-hover:bg-red-500"}
-				fillColor={props.fillColor}
-			/>
-			<span className={"self-center pl-1 text-xl group-hover:text-red-500"}>
-				{props.children}
-			</span>
-		</div>
-	)
-}
-
 export const PostFooter: FC<{
 	isLikedByUser: boolean
 	postWithUser: PostWithUser
@@ -42,15 +11,30 @@ export const PostFooter: FC<{
 	return (
 		<footer className="mt-3 flex text-gray-500">
 			<Link
-				className="mr-2 flex"
+				className="group mr-2 flex"
 				href={`/post/${postWithUser.author.username}/status/${postWithUser.post.id}`}
 			>
-				<ReplayIcon>{postWithUser.post.replaysCount}</ReplayIcon>
+				<div className={"flex rounded-full p-1 group-hover:bg-blue-400"}>
+					<Icon iconKind="chat" />
+				</div>
+				<span className={"self-center pl-1 text-xl group-hover:text-blue-400"}>
+					{postWithUser.post.replaysCount}
+				</span>
 			</Link>
-			<div onClick={() => console.log("click")}>
-				<LikeIcon fillColor={`${isLikedByUser ? "red" : ""}`}>
+
+			<div
+				className="group flex"
+				onClick={(e) => {
+					e.stopPropagation()
+				}}
+			>
+				<Heart
+					className={"h-9 w-9 rounded-full p-1 group-hover:bg-red-500"}
+					fillColor={isLikedByUser ? "red" : ""}
+				/>
+				<span className={"self-center pl-1 text-xl group-hover:text-red-500"}>
 					{postWithUser.post.likeCount}
-				</LikeIcon>
+				</span>
 			</div>
 		</footer>
 	)
