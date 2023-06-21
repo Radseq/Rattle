473b63036a54d36a3a4ac4f894fb783d0c6ee9af
Radseq <radek.gom@gmail.com>
2023-06-21 12:51:20 +0200

    fix: create PoolLengthOptions component for DRY

--- src/components/homePage/PoolContent.tsx
+++ src/components/homePage/PoolContent.tsx
@@ -1,9 +1,29 @@
-import { type FC } from "react"
+import { useState, type FC } from "react"
 import { PoolInput } from "./PoolInput"
 import { StyledLabel, StyledSelect } from "../styledHTMLElements/FloatingStyles"
 import { createAndIncrementFill } from "~/utils/helpers"
+import { PoolLength } from "./types"
+
+const PoolLengthOptions: FC<{ length: number; minLength?: number }> = ({
+	length,
+	minLength = 0,
+}) => {
+	return (
+		<>
+			{createAndIncrementFill(length, minLength).map((num, index) => {
+				return (
+					<option key={index} value={num}>
+						{num}
+					</option>
+				)
+			})}
+		</>
+	)
+}
 
 const PoolContent: FC<{ choise: string[] }> = ({ choise }) => {
+	let minPoolMinutes = 0
+	const [poolLength, setPoolLength] = useState<PoolLength>({ days: 1, hours: 0, minutes: 0 })
 	return (
 		<div className="mr-3 box-border w-full rounded-md border p-2">
 			{choise.map((_, index) => {
@@ -15,42 +35,24 @@ const PoolContent: FC<{ choise: string[] }> = ({ choise }) => {
 					<span>Pool length</span>
 				</div>
 				<div className="relative mr-4">
-					<StyledSelect id="days" className="w-20" defaultValue={0}>
-						{createAndIncrementFill(8).map((num, index) => {
-							return (
-								<option key={index} value={num}>
-									{num}
-								</option>
-							)
-						})}
+					<StyledSelect id="days" className="w-20" defaultValue={poolLength.days}>
+						<PoolLengthOptions length={8} />
 					</StyledSelect>
 					<StyledLabel htmlFor="days" side="left">
 						Days
 					</StyledLabel>
 				</div>
 				<div className="relative mr-4">
-					<StyledSelect id="hours" defaultValue={0} className="w-20">
-						{createAndIncrementFill(24).map((num, index) => {
-							return (
-								<option key={index} value={num}>
-									{num}
-								</option>
-							)
-						})}
+					<StyledSelect id="hours" defaultValue={poolLength.hours} className="w-20">
+						<PoolLengthOptions length={24} />
 					</StyledSelect>
 					<StyledLabel htmlFor="hours" side="left">
 						Hours
 					</StyledLabel>
 				</div>
 				<div className="relative">
-					<StyledSelect id="minutes" defaultValue={0} className="w-20">
-						{createAndIncrementFill(60).map((num, index) => {
-							return (
-								<option key={index} value={num}>
-									{num}
-								</option>
-							)
-						})}
+					<StyledSelect id="minutes" defaultValue={poolLength.minutes} className="w-20">
+						<PoolLengthOptions length={8} minLength={minPoolMinutes} />
 					</StyledSelect>
 					<StyledLabel htmlFor="minutes" side="left">
 						Minutes
