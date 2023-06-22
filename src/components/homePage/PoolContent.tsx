afc0e5671b42b20cf42b0dfcd0794eaa01f757ca
Radseq <radek.gom@gmail.com>
2023-06-22 19:48:07 +0200

    fix: handle value changes in selects of pool length
    protects for not allow pool length to be 0

--- src/components/homePage/PoolContent.tsx
+++ src/components/homePage/PoolContent.tsx
@@ -21,9 +21,47 @@ const PoolLengthOptions: FC<{ length: number; minLength?: number }> = ({
 	)
 }
 
+type PoolLengthExt = PoolLength & {
+	minPoolLength: number
+}
+// todo add to env instead hard coded
+const MIN_POOL_LENGTH = 5
+
 const PoolContent: FC<{ choise: string[] }> = ({ choise }) => {
-	const minPoolMinutes = 0
-	const [poolLength, setPoolLength] = useState<PoolLength>({ days: 1, hours: 0, minutes: 0 })
+	const [poolLength, setPoolLength] = useState<PoolLengthExt>({
+		days: 1,
+		hours: 0,
+		minutes: 0,
+		minPoolLength: 0,
+	})
+
+	const handlePoolLength = (type: "days" | "hours" | "minutes", value: number) => {
+		if (type === "hours") {
+			let minutes = poolLength.minutes
+			let minPoolLength = poolLength.minPoolLength
+			if (value === 0 && minutes < MIN_POOL_LENGTH && poolLength.days === 0) {
+				minutes = poolLength.minPoolLength
+				minPoolLength = MIN_POOL_LENGTH
+			} else {
+				minPoolLength = 0
+				minutes = poolLength.minutes
+			}
+			setPoolLength({ ...poolLength, hours: value, minutes: minutes, minPoolLength })
+		} else if (type === "days") {
+			let hours = poolLength.hours
+			if (value === 0 && hours === 0) {
+				hours = 1
+			}
+			setPoolLength({ ...poolLength, days: value, hours: hours })
+		} else {
+			let minutes = value
+			if (value === 0 && poolLength.hours === 0 && poolLength.days === 0) {
+				minutes = poolLength.minPoolLength
+			}
+			setPoolLength({ ...poolLength, minutes })
+		}
+	}
+
 	return (
 		<div className="mr-3 box-border w-full rounded-md border p-2">
 			{choise.map((_, index) => {
@@ -35,7 +73,12 @@ const PoolContent: FC<{ choise: string[] }> = ({ choise }) => {
 					<span>Pool length</span>
 				</div>
 				<div className="relative mr-4">
-					<StyledSelect id="days" className="w-20" defaultValue={poolLength.days}>
+					<StyledSelect
+						id="days"
+						className="w-20"
+						onChange={(e) => handlePoolLength("days", Number(e.target.value))}
+						value={poolLength.days}
+					>
 						<PoolLengthOptions length={8} />
 					</StyledSelect>
 					<StyledLabel htmlFor="days" side="left">
@@ -43,7 +86,12 @@ const PoolContent: FC<{ choise: string[] }> = ({ choise }) => {
 					</StyledLabel>
 				</div>
 				<div className="relative mr-4">
-					<StyledSelect id="hours" defaultValue={poolLength.hours} className="w-20">
+					<StyledSelect
+						id="hours"
+						onChange={(e) => handlePoolLength("hours", Number(e.target.value))}
+						value={poolLength.hours}
+						className="w-20"
+					>
 						<PoolLengthOptions length={24} />
 					</StyledSelect>
 					<StyledLabel htmlFor="hours" side="left">
@@ -51,8 +99,13 @@ const PoolContent: FC<{ choise: string[] }> = ({ choise }) => {
 					</StyledLabel>
 				</div>
 				<div className="relative">
-					<StyledSelect id="minutes" defaultValue={poolLength.minutes} className="w-20">
-						<PoolLengthOptions length={8} minLength={minPoolMinutes} />
+					<StyledSelect
+						id="minutes"
+						onChange={(e) => handlePoolLength("minutes", Number(e.target.value))}
+						value={poolLength.minutes}
+						className="w-20"
+					>
+						<PoolLengthOptions length={60} minLength={poolLength.minPoolLength} />
 					</StyledSelect>
 					<StyledLabel htmlFor="minutes" side="left">
 						Minutes
