c7e924b7120ce34349d6267529cf94c877ed3345
Radseq <radek.gom@gmail.com>
2023-07-31 12:29:49 +0200

    fix: changes in getAllUsersAndTags, changes first/last names into fullname and add user image url, result users which has username, full name

--- src/server/api/routers/features/search/search.ts
+++ src/server/api/routers/features/search/search.ts
@@ -1,13 +1,15 @@
 import { z } from "zod"
 import { clerkClient } from "@clerk/nextjs/server"
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc"
+import { getFullName } from "~/utils/helpers"
+import { type Profile } from "~/features/search"
 
 const MAX_USERS_COUNT = 20
 
 export const searchRouter = createTRPCRouter({
 	getAllUsersAndTags: publicProcedure.input(z.string()).query(async ({ input }) => {
 		if (!input) {
-			return ""
+			return null
 		}
 
 		const usersMatching = await clerkClient.users.getUserList({
@@ -18,12 +20,17 @@ export const searchRouter = createTRPCRouter({
 		//todo future search tags?
 		const searchedTags = [input]
 
-		const searchedProfiles = usersMatching.map((user) => {
-			return {
-				id: user.id,
-				username: user.username,
-				firstName: user.firstName,
-				lastName: user.lastName,
+		const searchedProfiles: Profile[] = []
+
+		usersMatching.forEach((user) => {
+			const fullName = getFullName(user.firstName, user.lastName)
+			if (user.username && fullName) {
+				searchedProfiles.push({
+					id: user.id,
+					username: user.username,
+					fullName,
+					imageUrl: user.profileImageUrl,
+				})
 			}
 		})
 
