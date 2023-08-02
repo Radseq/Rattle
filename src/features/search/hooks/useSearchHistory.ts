0756b0f5fcf083dcd29d8c51b91c3f3b9f8490aa
Radseq <radek.gom@gmail.com>
2023-08-02 13:41:02 +0200

    add: useSearchHistory hook for menage history in browser memory

---
+++ src/features/search/hooks/useSearchHistory.ts
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react"
+import { SearchResult, Profile } from "../types"
+
+type Observer = (history: SearchResult) => void
+
+const empty: SearchResult = {
+	searchedProfiles: [],
+	searchedTags: [],
+}
+
+const load = (): SearchResult => {
+	if (typeof window !== "undefined") {
+		const rawHistory = localStorage.getItem("search_history")
+		return rawHistory ? (JSON.parse(rawHistory) as SearchResult) : empty
+	}
+	return empty
+}
+const save = (history: SearchResult) => {
+	localStorage.setItem("search_history", JSON.stringify(history))
+}
+
+const observedHistory = (() => {
+	const observers = new Set<Observer>()
+
+	const value = load()
+	return {
+		subscribe: (observer: Observer) => {
+			observers.add(observer)
+			return () => {
+				observers.delete(observer)
+			}
+		},
+		add: (history: string | Profile) => {
+			if (typeof history === "object") {
+				if (!value.searchedProfiles.find((profile) => profile.id === history.id)) {
+					value.searchedProfiles = [...value.searchedProfiles, history]
+				}
+			} else {
+				if (!value.searchedTags.find((tag) => tag === history)) {
+					value.searchedTags = [...value.searchedTags, history]
+				}
+			}
+			save(value)
+			observers.forEach((observer) => observer(value))
+		},
+		remove: (entry: string | Profile) => {
+			if (typeof entry === "object") {
+				value.searchedProfiles = value.searchedProfiles.filter((it) => it.id !== entry.id)
+			} else {
+				value.searchedTags = value.searchedTags.filter((it) => it !== entry)
+			}
+			save(value)
+			observers.forEach((observer) => observer(value))
+		},
+		value,
+	} as const
+})()
+
+export const useSearchHistory = () => {
+	const [history, setHistory] = useState<SearchResult>(observedHistory.value)
+
+	useEffect(() => observedHistory.subscribe(setHistory), [])
+
+	return {
+		history,
+		add: (entry: string | Profile) => observedHistory.add(entry),
+		remove: (entry: string | Profile) => observedHistory.remove(entry),
+	} as const
+}
