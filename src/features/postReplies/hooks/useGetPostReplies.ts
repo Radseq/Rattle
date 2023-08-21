569f90a5bfa1fc0bc2350e7dd74c7a74a8c507e0
Radseq <radek.gom@gmail.com>
2023-08-21 19:43:16 +0200

    fix: missing changes in file (load configuration)

--- src/features/postReplies/hooks/useGetPostReplies.ts
+++ src/features/postReplies/hooks/useGetPostReplies.ts
@@ -1,11 +1,8 @@
 import { useEffect, useState } from "react"
 import { type PostWithAuthor } from "~/components/post/types"
+import { CONFIG } from "~/config"
 import { useLoadNextPage } from "~/features/homePage"
 import { api } from "~/utils/api"
-
-//todo move to config
-const POSTS_PER_PAGE = 10
-const SCROLL_THRESHOLD_IN_PX = 400
 
 export const useGetPostReplies = (postId: string, ulHeightInPx: number | null) => {
 	const [postReplies, setPostReplies] = useState<PostWithAuthor[]>()
@@ -13,14 +10,14 @@ export const useGetPostReplies = (postId: string, ulHeightInPx: number | null) =
 	const { data, fetchNextPage, refetch, isLoading } = api.posts.getPostReplies.useInfiniteQuery(
 		{
 			postId,
-			limit: POSTS_PER_PAGE - 1,
+			limit: CONFIG.POSTS_PER_PAGE - 1,
 		},
 		{
 			getNextPageParam: (lastPage) => lastPage.nextCursor,
 		}
 	)
 
-	const loadNextPosts = useLoadNextPage(SCROLL_THRESHOLD_IN_PX, ulHeightInPx)
+	const loadNextPosts = useLoadNextPage(CONFIG.SCROLL_THRESHOLD_IN_PX, ulHeightInPx)
 
 	useEffect(() => {
 		if (loadNextPosts) {
