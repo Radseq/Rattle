efdce215f4136abc60774bea47ed4d775f53eb5f
Radseq <radek.gom@gmail.com>
2023-08-21 19:59:08 +0200

    fix: load previous added config entities

--- src/features/profile/hooks/useGetPostsByAuthor.ts
+++ src/features/profile/hooks/useGetPostsByAuthor.ts
@@ -1,11 +1,8 @@
 import { useEffect, useState } from "react"
 import { type PostWithAuthor } from "~/components/post/types"
+import { CONFIG } from "~/config"
 import { useLoadNextPage } from "~/features/homePage"
 import { api } from "~/utils/api"
-
-//todo move to config
-const POSTS_PER_PAGE = 10
-const SCROLL_THRESHOLD_IN_PX = 400
 
 export const useGetPostsByAuthor = (authorId: string, ulHeightInPx: number | null) => {
 	const [posts, setPost] = useState<PostWithAuthor[]>()
@@ -13,13 +10,13 @@ export const useGetPostsByAuthor = (authorId: string, ulHeightInPx: number | nul
 	const { data, fetchNextPage, refetch, isLoading } = api.posts.getAllByAuthorId.useInfiniteQuery(
 		{
 			authorId,
-			limit: POSTS_PER_PAGE - 1,
+			limit: CONFIG.POSTS_PER_PAGE - 1,
 		},
 		{
 			getNextPageParam: (lastPage) => lastPage.nextCursor,
 		}
 	)
-	const loadNextPosts = useLoadNextPage(SCROLL_THRESHOLD_IN_PX, ulHeightInPx)
+	const loadNextPosts = useLoadNextPage(CONFIG.SCROLL_THRESHOLD_IN_PX, ulHeightInPx)
 
 	useEffect(() => {
 		if (loadNextPosts) {
