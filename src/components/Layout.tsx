f789965002b67071e31a0939945ec99ccfc1c622
Radseq <radek.gom@gmail.com>
2023-08-29 20:33:31 +0200

    fix: add main and section, fix: add missing Clerk's SignUp bg color

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,8 +11,8 @@ type LayoutProps = PropsWithChildren & {
 export const Layout = (props: LayoutProps) => {
 	const { user, isSignedIn } = useUser()
 	return (
-		<div className="m-auto flex justify-center 2xl:w-3/5">
-			<main
+		<main className="m-auto flex justify-center 2xl:w-3/5">
+			<section
 				className="h-full w-full grow border-x-2 border-gray-200 sm:w-full 
 								 md:mr-4 md:w-2/3 lg:p-4"
 			>
@@ -20,7 +20,7 @@ export const Layout = (props: LayoutProps) => {
 					<NavigationBar />
 				</header>
 				{props.children}
-			</main>
+			</section>
 			<aside className="hidden grow-0 sm:w-full md:block md:w-1/3">
 				<Panel>
 					<div className="sticky top-0 w-full p-4">
@@ -28,8 +28,8 @@ export const Layout = (props: LayoutProps) => {
 							<SignUp
 								appearance={{
 									elements: {
-										rootBox: "mx-auto pl-4 max-w-full",
-										card: "p-1 shadow-none max-w-full",
+										rootBox: "mx-auto pl-4 max-w-full bg-gray-200",
+										card: "p-1 shadow-none max-w-full bg-gray-200",
 									},
 								}}
 							/>
@@ -41,6 +41,6 @@ export const Layout = (props: LayoutProps) => {
 				</Panel>
 				{props.rightPanel}
 			</aside>
-		</div>
+		</main>
 	)
 }
