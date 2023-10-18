0ea6b97a3560fe4728c189a5c834ab5b8e09650b
Radseq <radek.g.10@gmail.com>
2023-10-18 18:31:58 +0200

    fix: small layout style adjustment

--- src/features/layout/components/Layout.tsx
+++ src/features/layout/components/Layout.tsx
@@ -14,16 +14,16 @@ export const Layout = (props: LayoutProps) => {
 	return (
 		<div className="flex">
 			<div className="m-auto flex">
-				<header className="min-w-16 bg-slate-600 xl:w-72">
+				<header className="w-16 min-w-[40px] xl:w-72">
 					<Menu isSignedIn={isSignedIn || false} />
 				</header>
-				<main className="mr-1 h-full w-full grow border-x-2 border-gray-200 sm:w-full md:w-2/3 lg:mr-4 lg:p-4">
+				<main className="mx-1 h-full w-full grow border-x-2 border-gray-200 sm:w-full md:w-2/3 lg:mx-4 lg:p-4">
 					<header className="sticky">
 						<NavigationBar />
 					</header>
 					{props.children}
 				</main>
-				<aside className="hidden grow-0 sm:w-full md:block md:w-96">
+				<aside className="hidden grow-0 md:block md:w-96">
 					<Panel>
 						<div className="sticky top-0 w-full md:p-2 lg:p-4">
 							{!isSignedIn && (
