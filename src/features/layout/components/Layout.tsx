cd1ec9c28404a5ae6a8b0d44624fdb9eb1191240
Radseq <radek.g.10@gmail.com>
2023-12-10 20:28:43 +0100

    fix: sticky position to left and right panel aka header, and aside

--- src/features/layout/components/Layout.tsx
+++ src/features/layout/components/Layout.tsx
@@ -15,16 +15,16 @@ export const Layout = (props: LayoutProps) => {
 	return (
 		<div className="flex">
 			<div className="m-auto flex">
-				<header className="w-16 min-w-[40px] xl:w-72">
+				<header className="sticky top-0 h-screen w-16 min-w-[40px] xl:w-72">
 					<Menu />
 				</header>
 				<main className="h-full w-full max-w-3xl grow border-gray-200 sm:mx-1 sm:w-full sm:border-x-2 md:w-2/3 lg:mx-4 lg:p-4">
 					{props.children}
 				</main>
 				<aside className="hidden grow-0 md:block md:w-96">
-					<SearchInput />
-					<Panel>
-						<div className="sticky">
+					<div className="sticky top-0">
+						<SearchInput />
+						<Panel>
 							{!isSignedIn && (
 								<SignUp
 									appearance={{
@@ -38,8 +38,8 @@ export const Layout = (props: LayoutProps) => {
 							{isSignedIn && user.firstName && user.username && (
 								<ProfilePanel firstName={user.firstName} username={user.username} />
 							)}
-						</div>
-					</Panel>
+						</Panel>
+					</div>
 					{props.rightPanel}
 				</aside>
 			</div>
