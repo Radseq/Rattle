2b4a31cb53129d5f81301c0339f5e425ae61aabb
Radseq <radek.g.10@gmail.com>
2023-12-10 20:32:58 +0100

    fix: badly looking main content of index page, temp solution

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,11 @@ import { type NextPage } from "next"
 import { Layout } from "~/features/layout"
 
 const Home: NextPage = () => {
-	return <Layout></Layout>
+	return (
+		<Layout>
+			<div className="w-96"></div>
+		</Layout>
+	)
 }
 
 export default Home
