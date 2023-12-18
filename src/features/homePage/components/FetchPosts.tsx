50473baa77f819dcb63b1c871aac09dd39deaca7
Radseq <radek.g.10@gmail.com>
2023-12-18 19:38:46 +0100

    refactor: FetchPost, create PostItem component to use logic there and connect with "view" PostItemForm component
    so PostItem is a place where logic from hook meet component without logic "View/UI"

--- src/features/homePage/components/FetchPosts.tsx
+++ src/features/homePage/components/FetchPosts.tsx
@@ -1,14 +1,10 @@
 import { type FC, useRef, useState } from "react"
 import { useGetHomePosts } from "../hooks"
-import { PostItemForm } from "~/components/post/PostItemForm"
-import { PostFooter } from "~/components/postsPage/PostFooter"
-import { PostContentSelector } from "~/components/post/PostContentSelector"
 import { type PostWithAuthor } from "~/components/post/types"
-import { getPostProfileType } from "~/utils/helpers"
 import { LoadingPage } from "~/components/LoadingPage"
 import { Dialog } from "~/components/dialog/Dialog"
 import { PostQuote } from "~/features/postQuote"
-import { usePostItemActions } from "../hooks/usePostItemActions"
+import { PostItem } from "./PostItem"
 
 export const FetchPosts: FC<{
 	signInUserId: string
@@ -18,11 +14,6 @@ export const FetchPosts: FC<{
 	const { isLoading, posts, refetch } = useGetHomePosts(
 		ulRef.current && ulRef.current.scrollHeight - ulRef.current.offsetTop
 	)
-
-	const { forwardPost, handlePostClick, likePost, pollVote, removePostForward, unlikePost } =
-		usePostItemActions(async () => {
-			await refetch()
-		})
 
 	const [quotePopUp, setQuotePopUp] = useState<PostWithAuthor | null>(null)
 
@@ -38,55 +29,15 @@ export const FetchPosts: FC<{
 		<>
 			<ul ref={ulRef}>
 				{posts?.map(({ author, post, signInUser }) => (
-					<PostItemForm
+					<PostItem
 						key={post.id}
-						createdPostTime={post.createdAt}
-						postAuthor={author}
-						onClickCapture={(clickCapture) => {
-							handlePostClick(clickCapture, post.id, author.username)
+						signInUserId={signInUserId}
+						postWithAuthor={{ author, post, signInUser }}
+						setQuotePopUp={(quoteData) => setQuotePopUp(quoteData)}
+						refetch={async () => {
+							await refetch()
 						}}
-						menuItemsType={getPostProfileType(
-							signInUser?.authorFollowed ?? false,
-							signInUserId,
-							author.id
-						)}
-					>
-						<PostContentSelector
-							post={post}
-							pollVote={(choiceId) =>
-								pollVote.mutate({
-									postId: post.id,
-									choiceId,
-								})
-							}
-						/>
-						<PostFooter
-							isForwarded={signInUser?.isForwarded ?? false}
-							onForwardClick={() => {
-								if (signInUser?.isForwarded ?? false) {
-									removePostForward.mutate(post.id)
-								} else {
-									forwardPost.mutate(post.id)
-								}
-							}}
-							onLikeClick={() => {
-								if (signInUser?.isLiked ?? false) {
-									unlikePost.mutate(post.id)
-								} else {
-									likePost.mutate(post.id)
-								}
-							}}
-							onQuoteClick={() => {
-								setQuotePopUp({ author, post, signInUser })
-							}}
-							sharedCount={post.quotedCount + post.forwardsCount}
-							isLiked={signInUser?.isLiked ?? false}
-							likeCount={post.likeCount}
-							username={author.username}
-							replyCount={post.replyCount}
-							postId={post.id}
-						/>
-					</PostItemForm>
+					/>
 				))}
 			</ul>
 			{quotePopUp && (
---
+++ src/features/homePage/components/PostItem.tsx
@@ -0,0 +1,72 @@
+import { type FC } from "react"
+import { PostContentSelector } from "~/components/post/PostContentSelector"
+import { PostFooter } from "~/components/postsPage/PostFooter"
+import { getPostProfileType } from "~/utils/helpers"
+import { usePostItemActions } from "../hooks/usePostItemActions"
+import { type PostWithAuthor } from "~/components/post/types"
+import { type ClickCapture, PostItemForm } from "~/components/post/PostItemForm"
+
+export const PostItem: FC<{
+	signInUserId: string
+	postWithAuthor: PostWithAuthor
+	setQuotePopUp: (quoteData: PostWithAuthor) => void
+	refetch: () => Promise<void>
+}> = ({ signInUserId, postWithAuthor, setQuotePopUp, refetch }) => {
+	const { author, post, signInUser } = postWithAuthor
+	const { forwardPost, handlePostClick, likePost, pollVote, removePostForward, unlikePost } =
+		usePostItemActions(async () => {
+			await refetch()
+		})
+
+	return (
+		<PostItemForm
+			key={post.id}
+			createdPostTime={post.createdAt}
+			postAuthor={author}
+			onClickCapture={(clickCapture: ClickCapture) => {
+				handlePostClick(clickCapture, post.id, author.username)
+			}}
+			menuItemsType={getPostProfileType(
+				signInUser?.authorFollowed ?? false,
+				signInUserId,
+				author.id
+			)}
+		>
+			<PostContentSelector
+				post={post}
+				pollVote={(choiceId) =>
+					pollVote.mutate({
+						postId: post.id,
+						choiceId,
+					})
+				}
+			/>
+			<PostFooter
+				isForwarded={signInUser?.isForwarded ?? false}
+				onForwardClick={() => {
+					if (signInUser?.isForwarded ?? false) {
+						removePostForward.mutate(post.id)
+					} else {
+						forwardPost.mutate(post.id)
+					}
+				}}
+				onLikeClick={() => {
+					if (signInUser?.isLiked ?? false) {
+						unlikePost.mutate(post.id)
+					} else {
+						likePost.mutate(post.id)
+					}
+				}}
+				onQuoteClick={() => {
+					setQuotePopUp({ author, post, signInUser })
+				}}
+				sharedCount={post.quotedCount + post.forwardsCount}
+				isLiked={signInUser?.isLiked ?? false}
+				likeCount={post.likeCount}
+				username={author.username}
+				replyCount={post.replyCount}
+				postId={post.id}
+			/>
+		</PostItemForm>
+	)
+}
